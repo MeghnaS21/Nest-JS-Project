2bb4bb1a77327b1f6f604216c517a34ad366b04c
MeghnaS21 <meghnasrivastava2000@gmail.com>
2022-10-11 21:05:42 +0530

    Nest Service provider

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,31 +1,35 @@
 import { Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { UserService } from './user.service';
 
 
 @Controller('/user')
-export class UserController{
-    @Get()
-    getUser(){
-        return 'I am from user Controller';
-    }
+export class UserController {
+  constructor(private userService: UserService) { };
+
+
+  @Get()
+  getUser() {
+    return this.userService.get();
+  }
 
   @Post()
-  store(@Req() req: Request){
-    return req.body;
+  store(@Req() req: Request) {
+    return this.userService.create(req);
   }
 
   @Patch('/:userId')
-  update(@Req() req: Request){
-    return req.body;
+  update(@Req() req: Request, @Param() param: {userId: number}) {
+    return this.userService.update(req, param);
   }
 
   @Get('/:userId')
-  getUserId(@Param() userId: number){
-    return userId;
+  getUserId(@Param() param: {userId: number}) {
+    return this.userService.show(param);
   }
 
   @Delete('/:userId')
-  deleteUser(@Param() userId: number){
-    return userId;
+  deleteUser(@Param() param: {userId: number}) {
+    return this.userService.delete( param);
   }
 }
