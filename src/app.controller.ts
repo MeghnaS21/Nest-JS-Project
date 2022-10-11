fbfed97d41c557f3abd74fb37763e463243da958
MeghnaS21 <meghnasrivastava2000@gmail.com>
2022-10-11 13:45:44 +0530

    Http requests and Nest Controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  store(@Req() req: Request){
+    return req.body;
+  }
+
+  @Patch('/:userId')
+  update(@Req() req: Request){
+    return req.body;
+  }
+
+  @Get('/:userId')
+  getUser(@Param() userId: number){
+    return userId;
+  }
+
+  @Delete('/:userId')
+  deleteUser(@Param() userId: number){
+    return userId;
   }
 }
